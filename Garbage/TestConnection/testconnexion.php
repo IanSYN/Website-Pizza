a2907a9a741bb217f07c71b5b590c53a6187f2f4
yannick HAN <yannick.han@outlook.com>
2023-12-08 13:42:04 +0100

    MAJ

---
+++ Garbage/TestConnection/testconnexion.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>test de connexion</title>
+</head>
+<body>
+    <?php
+    require_once ("./connexion.php");
+    connexion::connect();
+    ?>
+</body>
+</html>
