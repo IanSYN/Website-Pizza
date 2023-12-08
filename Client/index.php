fcff6072cca49d8e43e89ed03568012003dbb553
yannick HAN <yannick.han@outlook.com>
2023-12-08 15:20:43 +0100

    MAJ

---
+++ Client/index.php
@@ -0,0 +1,34 @@
+<DOCTYPE!>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Pizzavers</title>
+    <link rel="stylesheet" href="css/StyleSheet.css">
+</head>
+<header>
+    <div class="bTache">
+        <h1 class="title">
+            Pizzavers
+        </h1>
+        <nav>
+            <ul>
+                <li>
+                    <a href="index.php">HOME</a>
+                </li>
+                <li>
+                    <a href="menu.php">PRODUIT</a>
+                </li>
+                <li>
+                    <a href="contact.php">QUI SOMMES NOUS ?</a>
+                </li>
+                <li>
+                    <a href="panier.php">PANIER</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</header>
+<body>
+    <div class="EnAvant">
+    </div>
+</body>
