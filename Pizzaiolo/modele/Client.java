ced90c14baf01b789c58e934c69aa32f03d02c93
SasaMymy <124688645+SasaMymy@users.noreply.github.com>
2023-12-08 13:35:31 +0100

    Add files via upload

---
+++ Pizzaiolo/modele/Client.java
@@ -0,0 +1,10 @@
+package modele;
+
+public class Client {
+    int idClient;
+    String mailClient;
+    String mdpClient;
+    String nomClient;
+    String prenomClient;
+    String adresseClient;
+}
