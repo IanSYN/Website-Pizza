b8acea5495ccbfbf6211db20f79e4daa11d5e7d0
SasaMymy <124688645+SasaMymy@users.noreply.github.com>
2023-12-13 18:23:30 +0100

    Update send_email.php

--- DB/send_email.php
+++ DB/send_email.php
@@ -1,8 +1,22 @@
-<?php
+/*<?php
     $destinataire = $argv[1];
     $expediteur = $argv[2];
     $objet = $argv[3];
     $texte = $argv[4];
 
     mail($destinataire, $objet, $texte ,$expediteur);
+?>*/
+
+<?php
+$to = "sarahmy.messaoudi@gmail.com";
+$subject = "Test Email";
+$message = "This is a test email.";
+
+$headers = "From: sarahmy.messaoudi@gmail.com\r\n";
+
+if (mail($to, $subject, $message, $headers)) {
+    echo "Email sent successfully.";
+} else {
+    echo "Failed to send email.";
+}
 ?>
