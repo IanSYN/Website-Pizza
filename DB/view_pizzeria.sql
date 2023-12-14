688b724387dd7f0972b1eb1e48c5ade503288ade
SasaMymy <124688645+SasaMymy@users.noreply.github.com>
2023-12-14 18:25:25 +0100

    Update view_pizzeria.sql

--- DB/view_pizzeria.sql
+++ DB/view_pizzeria.sql
@@ -1,5 +1,5 @@
 -- Cette vue permet d'afficher les produits du catalogue, avec leurs prix et leurs catégories
-CREATE VIEW vCatalogue AS
+CREATE OR REPLACE VIEW VCatalogue AS
     SELECT C.idCategorie, C.nomCategorie, P.nomProduit, P.prixProduit, P.coverProduit
     FROM `Categorie` C
     INNER JOIN `Produit` P ON C.idCategorie = P.idCategorie;
