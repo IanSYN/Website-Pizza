3e4d891a4df119422cfa0d45bdb644c415594924
SasaMymy <124688645+SasaMymy@users.noreply.github.com>
2023-12-14 17:46:31 +0100

    Update view_pizzeria.sql

--- DB/view_pizzeria.sql
+++ DB/view_pizzeria.sql
@@ -3,3 +3,33 @@ CREATE VIEW vCatalogue AS
     SELECT C.idCategorie, C.nomCategorie, P.nomProduit, P.prixProduit, P.coverProduit
     FROM `Categorie` C
     INNER JOIN `Produit` P ON C.idCategorie = P.idCategorie;
+
+CREATE OR REPLACE VIEW VPizza 
+AS 
+	(SELECT Pizza.idPizza, nomProduit, nomTaille, coverProduit FROM Pizza
+	inner join Produit on Produit.idProduit = Pizza.idProduit
+	inner join Taille on Taille.idTaille = Pizza.idTaille
+	GROUP BY Pizza.idPizza, nomProduit, nomTaille);
+
+CREATE OR REPLACE VIEW VPizzaioloCommande
+AS
+	(SELECT idCommande, nomProduit, nomTaille, nomIngredient, quantitePizza, quantiteIngredient*quantiteSupplement as qntProduitPizza FROM Pizza
+	inner join Produit on Produit.idProduit = Pizza.idProduit
+	inner join Taille on Taille.idTaille = Pizza.idTaille
+	natural join Base
+ 	natural join Ingredient
+	natural join PizzaPersonnalisee
+	natural join Supplement
+	GROUP BY idCommande, nomProduit, nomTaille, nomIngredient, quantitePizza, qntProduitPizza);
+
+
+CREATE OR REPLACE VIEW VPizzaIngr
+AS
+	(SELECT nomPizza, nomIngredient, nomTaille, coverIngredient, nomAllergene, prixProduit from Ingredient
+	natural join Allergene 
+	inner join Base on Base.idIngredient = Ingredient.idIngredient
+	inner join Pizza on Pizza.idPizza = Base.idPizza
+	inner join Taille on Taille.idTaille = Pizza.idTaille
+	inner join Produit on Produit.idProduit = Pizza.idProduit
+	GROUP BY nomPizza, nomIngredient, nomTaille, coverIngredient, nomAllergene, prixProduit);
+	
