c635525611ad6e0117e6372739b40e23a1b247a7
SasaMymy <124688645+SasaMymy@users.noreply.github.com>
2023-12-19 11:44:53 +0100

    Add files via upload

--- Pizzaiolo/modele/Commande.java
+++ Pizzaiolo/modele/Commande.java
@@ -1,63 +1,150 @@
-package Modele;
+package modele;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 
-import Modele.Adresse;
-import Modele.Produit;
-
 public class Commande {
-    // ***********************************
-	// ******* ATTRIBUTS *****************
-	// ***********************************
-
-    private int IdCommande;
-    private float PrixCommandeTotal;
-    private float tempsRestant;
+    private double tempsRestant;
     private Adresse adresseArrivee;
     private ArrayList<Produit> laCommande;
     private boolean ready = false;
-    //private ArrayList<Commande> commandePrete;
+    //ArrayList<Commande> commandePrete;
     private float ratio;
     private LocalDateTime dateCommande;
     private int numCommande;
 
-    // ***********************************
-    // ******* CONSTRUCTEURS *************
-    // ***********************************
-
-    public Commande(Adresse adresseArrivee, ArrayList<Produit> commande) {
+    //Constructeur 
+    public Commande(int numCommande, Adresse adresseArrivee, double tempsRest, ArrayList<Produit> laCommande) {
+        this.numCommande = numCommande;
         this.adresseArrivee = adresseArrivee;
-        this.laCommande = commande;
+        this.tempsRestant = tempsRest;
+        this.laCommande = laCommande;
         this.dateCommande = LocalDateTime.now();
     }
 
-    //Constructeur test
-    public Commande(){
-        this.adresseArrivee = new Adresse(ratio, numCommande);
-        this.laCommande = new ArrayList<Produit>();
+    public Commande(int numCommande) {
+        this.numCommande = numCommande;
+        this.adresseArrivee = null;
+        this.laCommande = null;
         this.dateCommande = LocalDateTime.now();
     }
 
-
-    // ***********************************
-    // ******* GETTERS ET SETTERS ********
-    // ***********************************
-
-    public int getIdCommande(){
-        return this.IdCommande;
+    // Getters and Setters
+    public double getTempsRestant() {
+        return tempsRestant;
     }
 
-    public float getPrixCommandeTotal(){
-        return this.PrixCommandeTotal;
+    public void setTempsRestant(float tempsRestant) {
+        this.tempsRestant = tempsRestant;
     }
 
-    // ***********************************
-    // ******* METHODES ******************
-    // ***********************************
+    public Adresse getAdresseArrivee() {
+        return adresseArrivee;
+    }
 
-    public String indexCommandeNom(int i){
-        return this.laCommande.get(i).getNomProduit();
+    public void setAdresseArrivee(Adresse adresseArrivee) {
+        this.adresseArrivee = adresseArrivee;
+    }
+
+    public ArrayList<Produit> getLaCommande() {
+        return laCommande;
+    }
+
+    public void setLaCommande(ArrayList<Produit> laCommande) {
+        this.laCommande = laCommande;
+    }
+
+    public boolean isReady() {
+        return ready;
+    }
+
+    public void setReady(boolean ready) {
+        this.ready = ready;
+    }
+
+    public float getRatio() {
+        return ratio;
+    }
+
+    public void setRatio(float ratio) {
+        this.ratio = ratio;
+    }
+
+    public LocalDateTime getDateCommande() {
+        return dateCommande;
+    }
+
+    public void setDateCommande(LocalDateTime dateCommande) {
+        this.dateCommande = dateCommande;
+    }
+
+    public int getIntCommande(){
+        return numCommande;
+    }
+
+    public int getNumCommande() {
+        return numCommande;
+    }
+
+    public void setNumCommande(int numCommande) {
+        this.numCommande = numCommande;
+    }
+
+    //methode
+
+    public String toString() {
+        String resultat = "";
+
+        for (int i = 0; i< laCommande.size(); i++){
+            resultat += laCommande.get(i).toString() + ", ";
+        }
+
+        resultat = resultat.substring(0, resultat.length() - 2); // on retire les deux derniers caractères pour enlever la dernière virgule
+
+        return resultat;
+    }
+
+
+    public Commande calcRatio(float tempsRestant, Adresse ad1) {
+        //float distance;
+        Adresse ad2 = adresseArrivee;
+        this.ratio = tempsRestant / calcDistance(ad1, ad2);
+        return null;
+    }
+
+    public Duration calcDureeRestante(LocalDateTime dateCom, LocalDateTime sysDate) {
+        sysDate = LocalDateTime.now();
+        return Duration.between(dateCom, sysDate);
+    }
+
+    public LocalDateTime dateHeureFinal(LocalDateTime dateCom, LocalDateTime sysDate) {
+        sysDate = LocalDateTime.now();
+        return LocalDateTime.now().minus(calcDureeRestante(dateCom, sysDate));
+    }
+
+    public void prete() {
+        this.ready = true;
+    }
+
+    public float calcDistance(Adresse adresseArrivee, Adresse adresseDepart) {
+        final int R = 6371; // Rayon de la Terre en kilomètres
+
+        double lat1 = Math.toRadians(adresseDepart.getLatitude());
+        double lon1 = Math.toRadians(adresseDepart.getLongitude());
+        double lat2 = Math.toRadians(adresseArrivee.getLatitude());
+        double lon2 = Math.toRadians(adresseArrivee.getLongitude());
+
+        double dLat = lat2 - lat1;
+        double dLon = lon2 - lon1;
+
+        double a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+                   Math.cos(lat1) * Math.cos(lat2) *
+                   Math.sin(dLon / 2) * Math.sin(dLon / 2);
+
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+        float distance = (float) (R * c); // La distance résultante en kilomètres
+        return distance;
     }
 }
-
---
+++ Pizzaiolo/modele/Livreur.java
@@ -0,0 +1,125 @@
+package modele;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import modele.Commande;
+
+public class Livreur {
+    private final static int MAXPLACE = 5;
+    ArrayList<Commande>cargo = new ArrayList<Commande>(MAXPLACE);
+
+    // Constructeur
+
+    //setter et getter
+
+    public ArrayList<Commande> getCargo() {
+        return cargo;
+    }
+
+    // Setter pour l'ArrayList cargo
+    public void setCargo(ArrayList<Commande> cargo) {
+        this.cargo = cargo;
+    }
+
+
+    //Méthodes
+    public void addCargo(Commande c){
+        cargo.add(c);
+    }
+
+    public void removeCargo(int pos){
+        cargo.remove(pos);
+    }
+
+    public void TrajTotal(){
+        //Affiche la distance entre les deux adresses
+    }
+
+    //**Note : Le livreur n'a pas le choix de sa livraison**/
+    /*première commande = temps */
+    /* 2 emes commande = ratio temps / distance par rapport a commande 1 */
+    /* repeat until 5 commandes */
+
+    public Commande tempsMin(ArrayList<Commande> c) {
+        if (c.isEmpty()) {
+            return null; // Renvoyer null si la liste est vide
+        }
+    
+        Commande minCommande = c.get(0); // Supposons que le premier élément a le temps restant minimum
+    
+        for (Commande commande : c) {
+            if (commande.getTempsRestant() < minCommande.getTempsRestant()) {
+                minCommande = commande;
+            }
+        }
+    
+        return minCommande;
+    }
+
+    public Commande meilleurRatio(ArrayList<Commande> c) {
+        if (c.isEmpty()) {
+            return null; // Renvoyer null si la liste est vide
+        }
+    
+        Commande meilleureCommande = c.get(0); // Supposons que le premier élément a le meilleur ratio
+    
+        for (Commande commande : c) {
+            if (commande.getRatio() > meilleureCommande.getRatio()) {
+                meilleureCommande = commande;
+            }
+        }
+    
+        return meilleureCommande;
+    }
+
+    public void DynamicPos(ArrayList<Commande> c){
+        //Algorithme glouton pour trouver la position optimale
+        while(!cargo.isEmpty()){
+            for (Commande commande : c) {
+                if(commande.getTempsRestant() <= 0){
+                    DynamicNeg(c);
+                }
+                else if(cargo.isEmpty()){
+                    cargo.add(tempsMin(c));
+                    DynamicPos(c);
+                }
+                else{
+                    cargo.add(meilleurRatio(c));
+                }
+            }
+        }
+    }
+
+    //a ameliorer (pour l'instant ça ajoute une commande dans le cargo des que le temps restant passe au negatif)
+    private void DynamicNeg(ArrayList<Commande> c) {
+         while(!cargo.isEmpty()){
+            for (Commande commande : c) {
+                if(commande.getTempsRestant() <= 0){
+                    if(commande.getTempsRestant() <= -15.00){
+                        c.add(commande);
+                        DynamicNeg(c);
+                    }
+                }else{
+                    DynamicPos(c);
+                }
+            }
+        }
+
+    }
+
+    public void afficherCargo(ArrayList<Commande> c) {
+        System.out.println("Detail de livraison pour chaques Commande dans le Cargo :");
+        
+        for (Commande commande : c) {
+            System.out.println("Numéro de commande : " + commande.getNumCommande());
+            System.out.println("Temps restant : " + commande.getTempsRestant());
+            System.out.println("L'adresse de livraison : " + commande.getAdresseArrivee());
+            System.out.println("------------------------");
+        }
+    }
+
+    
+
+    
+}
