f669f03dd85c7060cef8cb42b60d05061bc7eed3
SasaMymy <124688645+SasaMymy@users.noreply.github.com>
2023-12-22 10:07:32 +0100

    Update view_pizzeria.sql

--- DB/view_pizzeria.sql
+++ DB/view_pizzeria.sql
@@ -70,6 +70,6 @@ AS
 
 CREATE OR REPLACE VIEW VPizzaPersonnalisee
 AS 
-	(SELECT pp.idPizzaPersonnalisee, idPizza, idCommande, idIngredient, quantitePizza  from PizzaPersonnalisee pp
+	(SELECT pp.idPizzaPersonnalisee, idPizza, idCommande, idIngredient, quantitePizza, quantiteSupplement  from PizzaPersonnalisee pp
 	inner join Supplement s on s.idPizzaPersonnalisee = pp.idPizzaPersonnalisee
 	group by pp.idPizzaPersonnalisee, idPizza, idCommande, idIngredient, quantitePizza);
