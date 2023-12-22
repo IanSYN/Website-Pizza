1e719266656fe791e6c411074c6feb5eed0e3f48
SasaMymy <124688645+SasaMymy@users.noreply.github.com>
2023-12-22 10:03:46 +0100

    Update view_pizzeria.sql

--- DB/view_pizzeria.sql
+++ DB/view_pizzeria.sql
@@ -53,7 +53,23 @@ AS
 
 CREATE OR REPLACE VIEW VCommande
 AS
-	(SELECT idCommande, idPanier, idProduit, idPizzaPersonnalisee, quantiteProduit, quantitePizza From Commande c
-	inner join Panier p on p.idCommande = c.idCommande
+	(SELECT c.idCommande, idProduit, idPizzaPersonnalisee, quantiteProduit, quantitePizza From Commande c
+    	inner join Panier p on p.idCommande = c.idCommande
+	left join  PizzaPersonnalisee pp on pp.idCommande = c.idCommande
+   	 union  SELECT c.idCommande, idProduit, idPizzaPersonnalisee, quantiteProduit, quantitePizza From Commande c
+   	 inner join Panier p on p.idCommande = c.idCommande
+	right join PizzaPersonnalisee pp on pp.idCommande = c.idCommande
+    	group by idCommande);
+
+CREATE OR REPLACE VIEW VPanierGlobal
+AS 
+	(SELECT idProduit, idPizzaPersonnalisee, quantiteProduit, quantitePizza from Panier p
+	natural join Commande c 
 	inner join PizzaPersonnalisee pp on pp.idCommande = c.idCommande
-	group by idCommande, idPanier, idProduit, idPizzaPersonnalisee, quantiteProduit, quantitePizza);
+	group by idProduit, idPizzaPersonnalisee, quantiteProduit, quantitePizza);
+
+CREATE OR REPLACE VIEW VPizzaPersonnalisee
+AS 
+	(SELECT idPizzaPersonnalisee, idPizza, idCommande, idIngredient, quantitePizza  from PizzaPersonnalisee pp
+	inner join supplement s on s.idPizzaPersonnalisee = pp.idPizzaPersonnalisee
+	group by idPizzaPersonnalisee, idPizza, idCommande, idIngredient, quantitePizza);
