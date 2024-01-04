3fdc9cf11f76fde3354059d282d44ef8919fd8ad
Yannick HAN <134492304+S3eraph@users.noreply.github.com>
2024-01-04 13:41:45 +0100

    Delete Client directory

--- Client/controller/controllerPanier.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once('controllerDefaut.php');
-require_once('model/Panier.php');
-class controllerPanier extends controllerDefaut{
-
-    protected static $identifiant = "idCommande";
-    protected static $classe = 'Panier';
-}
-?>
