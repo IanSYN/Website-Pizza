4f208aa2268e9846edaf5aeae05ba6ce3f0fbbbc
SasaMymy <124688645+SasaMymy@users.noreply.github.com>
2024-01-08 11:02:59 +0100

    Update view_pizzeria.sql

--- DB/view_pizzeria.sql
+++ DB/view_pizzeria.sql
@@ -55,7 +55,7 @@ AS
 
 CREATE OR REPLACE VIEW VAllergenePizza
 AS 
-	(SELECT idAllergene, idPizza, nomAllergene
+	(SELECT A.idAllergene, idPizza, nomAllergene
 	FROM Allergene A
 	INNER JOIN Ingredient I on I.idAllergene = A.idAllergene
 	INNER JOIN Base B on B.idIngredient = I.idIngredient);
