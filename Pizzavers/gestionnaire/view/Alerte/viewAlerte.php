eca5b8c8c7e5aa3ca4436c4e69e11578c181ad59
marcroussel <marc.roussel04@outlook.fr>
2024-01-09 11:08:26 +0100

    Co-authored-by: SasaMymy <SasaMymy@users.noreply.github.com>

--- Pizzavers/gestionnaire/view/Alerte/viewAlerte.php
+++ Pizzavers/gestionnaire/view/Alerte/viewAlerte.php
@@ -1,6 +1,6 @@
 <div class='alerte'>
 <?php
-    echo "<img src='img/$cover' />";
+    echo "<img src='img/ingredient/$cover' />";
     echo "<h3> Stock manquant de $nom </h3>";
     echo "<p> Stock actuel : $stock g </p>";
 ?>
