8bfb3ce84936343cb9170a0bf89d6754f12470e0
marcroussel <marc.roussel04@outlook.fr>
2024-01-09 21:06:04 +0100

    bruh

--- Pizzavers/model/Ingredient.php
+++ Pizzavers/model/Ingredient.php
@@ -1,14 +1,6 @@
 <?php
 require_once('objet.php');
 
-<<<<<<< HEAD
-class Ingredient extends objet
-{
-    protected static $identifiant = "idIngredient";
-
-    protected static $classe = 'Ingredient';
-=======
->>>>>>> 884e0727b3314dc160275d21710a06265a2b161e
 class Ingredient extends objet {
     // Partie statique
     protected static $identifiant = "idIngredient";
@@ -17,41 +9,6 @@ class Ingredient extends objet {
     // Partie attributs
     protected int $idIngredient;
     protected string $nomIngredient;
-<<<<<<< HEAD
-    protected string $prixIngredient;
-    protected string $coverIngredient;
-
-    //constructeur
-    public function __construct($idIngredient = null, $nomIngredient = null, $prixIngredient = null, $coverIngredient = null) {
-        if(!is_null($idIngredient)){
-            $this->idIngredient = $idIngredient;
-            $this->nomIngredient = $nomIngredient;
-            $this->prixIngredient = $prixIngredient;
-            $this->coverIngredient = $coverIngredient;
-        }
-    }
-
-    //methode to string
-    public function __toString()
-    {
-        return $this->nomIngredient . "(" . $this->idIngredient . ")";
-    }
-
-    public static function getIngreAll(){
-        $classRecuperee = static::$classe;
-        $requete = "SELECT * FROM $classRecuperee;";
-        $resultat = connexion::pdo()->query($requete);
-        try{
-            $resultat->execute();
-            $resultat->setFetchMode(PDO::FETCH_CLASS, $classRecuperee);
-            $tab = $resultat->fetchAll();
-            return $tab;
-        }
-        catch(PDOException $e){
-            echo $e->getMessage();
-            return false;
-=======
->>>>>>> 884e0727b3314dc160275d21710a06265a2b161e
     protected float $stockIngredient;
     protected float $prixIngredient;
     protected string $coverIngredient;
